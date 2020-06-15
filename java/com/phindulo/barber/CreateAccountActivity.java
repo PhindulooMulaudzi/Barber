31ba6731d5506c82c95d6c7c4c17735febfa8ed7
Phindulo <phinduloomulaudzi@gmail.com>
2020-06-15 21:43:38 +0200

    Added loging activity and account activity, pending implementation

--- java/com/phindulo/barber/CreateAccountActivity.java
+++ java/com/phindulo/barber/CreateAccountActivity.java
@@ -2,13 +2,28 @@ package com.phindulo.barber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    private Button createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+
+        createAccount = findViewById(R.id.create_account_button);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateAccountActivity.this, DashboardActivity.class));
+            }
+        });
     }
 }
